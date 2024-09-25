a2ee279b4b49a34caf07631efede56b0e1df1ea0
Vuthibichngoc212 <96900628+Vuthibichngoc212@users.noreply.github.com>
2024-09-25 16:35:15 +0700

    fix regex phone

--- src/helpers/validationSchema.ts
+++ src/helpers/validationSchema.ts
@@ -6,10 +6,7 @@ export const validationSchema = yup.object().shape({
   phone: yup
     .string()
     .required("Số điện thoại không được để trống")
-    .matches(
-      /^(0)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$/,
-      "Số điện thoại không hợp lệ"
-    ),
+    .matches(/^(84|0[3|5|7|8|9])[0-9]{8}$/, "Số điện thoại không hợp lệ"),
   address: yup.string().required("Địa chỉ không được để trống"),
   email: yup
     .string()
